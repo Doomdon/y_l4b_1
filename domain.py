07543109bb3656823c97497eb6d3e466fee23064
Doomdon <2andrusha3@gmail.com>
2022-06-23 21:27:48 +0300

    #1

---
+++ domain.py
@@ -0,0 +1,25 @@
+import re
+
+
+''' Сравниваем url с регулярным выражением и записываем в переменную. После меняем символы. '''
+
+
+def domain_name(url):
+    result = re.search(r'[./]?(?:www.)?\w+\-?\w+[.]', url)
+    s = result[0]
+    s = s.replace('www', '')
+    s = s.replace('.', '')
+    s = s.replace('/', '')
+    return s
+
+
+# тесты
+assert domain_name("http://google.com") == "google"
+assert domain_name("http://google.co.jp") == "google"
+assert domain_name("www.xakep.ru") == "xakep"
+assert domain_name("https://youtube.com") == "youtube"
+
+print(domain_name("http://google.com"))
+print(domain_name("http://google.co.jp"))
+print(domain_name("www.xakep.ru"))
+print(domain_name("https://youtube.com"))
