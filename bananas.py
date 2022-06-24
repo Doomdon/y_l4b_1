d62825bc6944188057132d8dcbe2426906fd6620
Doomdon <2andrusha3@gmail.com>
2022-06-24 20:20:16 +0300

    #4

--- bananas.py
+++ bananas.py
@@ -1,10 +1,22 @@
 import itertools
 
+
 def bananas(s) -> set:
-    result = set(itertools.combinations(s, len(s)))
-    strstr = [str(item) for item in itertools.chain(result)]
-    return "".join(strstr)
+    result = set()
+    for j in itertools.combinations(range(len(s)), len(s) - 6):
+        b = list(s)
+        for i in j: b[i] = '-'
+        b = ''.join(b)
+        if b.replace('-', '') == "banana":
+            result.add(b)
+    return result
 
 
-print((bananas("banana")))
-print(bananas("bbananana"))
+# тесты
+assert bananas("banann") == set()
+assert bananas("banana") == {"banana"}
+assert bananas("bbananana") == {"b-an--ana", "-banana--", "-b--anana", "b-a--nana", "-banan--a",
+                                "b-ana--na", "b---anana", "-bana--na", "-ba--nana", "b-anan--a",
+                                "-ban--ana", "b-anana--"}
+assert bananas("bananaaa") == {"banan-a-", "banana--", "banan--a"}
+assert bananas("bananana") == {"ban--ana", "ba--nana", "bana--na", "b--anana", "banana--", "banan--a"}
